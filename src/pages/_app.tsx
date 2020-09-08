ef88c697c23d596341ade196d90aa0064ff335e8
Shun Nakano <nakano_syun@waku-2.com>
2020-09-08 16:00:05 +0900

    _app作成

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { AppProps } from 'next/app';
+import { useStore } from '../redux/store';
+
+import '../components/stylesheets/reset.scss';
+
+export default function App({
+	Component,
+	pageProps,
+}: AppProps): React.ReactElement {
+	const store = useStore(pageProps.initialReduxState);
+
+	/* eslint-disable react/jsx-props-no-spreading */
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	);
+}
