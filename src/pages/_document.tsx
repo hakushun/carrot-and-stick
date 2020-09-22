4b389364b38264c1efd3ef18d7e384f6b6b0629d
Shun Nakano <hakushun.pianist@gmail.com>
2020-09-22 21:37:55 +0900

    google font, font awesomeの読み込み

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-	render() {
+	render(): JSX.Element {
 		return (
 			<Html lang="ja">
 				<Head />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+				<script
+					src="https://kit.fontawesome.com/1fb0847578.js"
+					crossOrigin="anonymous"></script>
 				<body>
 					<Main />
 					<NextScript />
