7237b67ae8d39621c037c9f04862de07a2994927
Shun Nakano <hakushun.pianist@gmail.com>
2020-09-22 21:38:51 +0900

    modules/signInにvalidationとselectorの追加

--- src/redux/modules/signIn.ts
+++ src/redux/modules/signIn.ts
@@ -1,11 +1,10 @@
 import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { createSelector } from 'reselect';
+import { combine, maxLength, minLength, regexp } from 'favalid';
 import { RootState } from './reducers';
 
-const actionCreator = actionCreatorFactory();
-
-type SignIn = {
+export type SignIn = {
 	form: {
 		mailAddress: string;
 		password: string;
@@ -28,6 +27,7 @@ type ChangePayload = {
 /**
  * action
  */
+const actionCreator = actionCreatorFactory();
 export const changeSignInForm = actionCreator<ChangePayload>(
 	'CHANGE_SIGN_IN_VALUE',
 );
@@ -48,6 +48,34 @@ const INITIAL_STATE = {
 };
 
 /**
+ * validations
+ */
+
+const minEmailLengthMessage = () => '10文字以上で入力してください';
+const maxEmailLengthMessage = () => '100文字以内で入力してください';
+const minPasswordLengthMessage = () => '6文字以上で入力してください';
+const maxPasswordLengthMessage = () => '16文字以内で入力してください';
+const emailMessage = () => 'メールアドレスの形式が不正です。';
+/* eslint-disable-next-line */
+const mailCheck = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+const emailValidator = combine(
+	minLength(10, minEmailLengthMessage),
+	maxLength(100, maxEmailLengthMessage),
+	regexp(mailCheck, emailMessage, {}),
+);
+
+const passwordValidator = combine(
+	minLength(6, minPasswordLengthMessage),
+	maxLength(16, maxPasswordLengthMessage),
+);
+
+const validator = (form: typeof INITIAL_STATE.form) => ({
+	mailAddress: emailValidator(form.mailAddress),
+	password: passwordValidator(form.password),
+});
+
+/**
  * reducer
  */
 const reducer = reducerWithInitialState(INITIAL_STATE).case(
@@ -62,7 +90,44 @@ const reducer = reducerWithInitialState(INITIAL_STATE).case(
 			...state.edited,
 			[payload.name]: true,
 		},
+		validationErrors: { ...validator(state.form) },
 	}),
 );
-
 export default reducer;
+
+/**
+ * selector
+ */
+// createSelector：storeから欲しいstateを持ってくる、stateをここで加工できるよ
+export const selectSignIn = createSelector(
+	[(state: RootState) => state.signIn],
+	(signIn) => signIn,
+);
+
+export const selectSignInMailAdressErrors = createSelector(
+	[(state: RootState) => state.signIn],
+	(signIn) => {
+		return (
+			signIn.edited.mailAddress && signIn.validationErrors.mailAddress.error
+		);
+	},
+);
+
+export const selectSignInPasswordErrors = createSelector(
+	[(state: RootState) => state.signIn],
+	(signIn) => {
+		return signIn.edited.password && signIn.validationErrors.password.error;
+	},
+);
+
+export const selectSignInValidationErrors = createSelector(
+	[(state: RootState) => state.signIn],
+	(signIn) => {
+		return (
+			!signIn.edited.mailAddress ||
+			!signIn.edited.password ||
+			signIn.validationErrors.mailAddress.error ||
+			signIn.validationErrors.password.error
+		);
+	},
+);
