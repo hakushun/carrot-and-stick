77ffabe0e007813894e43b245501c06d9149d2f8
Shun Nakano <nakano_syun@waku-2.com>
2020-10-01 17:35:18 +0900

    modules/missionの実装

--- src/redux/modules/mission.ts
+++ src/redux/modules/mission.ts
@@ -4,16 +4,60 @@ import { createSelector } from 'reselect';
 import { RootState } from './reducers';
 
 type MissionStatus = 'new' | 'progress' | 'complete';
-type Mission = {
+export type Mission = {
 	id: number;
 	title: string;
 	dueDate: string;
 	mentalDamage: number;
 	pysicalDamage: number;
 	point: number;
-	memo?: string[];
+	memo: string[];
 	status: MissionStatus;
-	registarDate: string;
-	completeDate: string;
-	heroInterview?: string;
+	registarDate: number;
+	completeDate: number;
+	heroInterview: string;
 };
+
+type ChangePayload = {
+	name: string;
+	value: string;
+};
+
+/**
+ * action
+ */
+const actionCreator = actionCreatorFactory();
+const change = actionCreator<ChangePayload>('CHANGE_MISSION');
+
+const INITIAL_STATE = {
+	id: 0,
+	title: '',
+	mentalPoint: 0,
+	pysicalPoint: 0,
+	totalPoint: 0,
+	memo: '',
+	status: 'new',
+	registarDate: 0,
+	usedDate: 0,
+};
+
+/**
+ * reducer
+ */
+const reducer = reducerWithInitialState(INITIAL_STATE).case(
+	change,
+	(state, payload) => ({
+		...state,
+		[payload.name]: payload.value,
+	}),
+);
+
+export default reducer;
+
+/**
+ * selector
+ */
+export const selectMission = createSelector(
+	[(state: RootState) => state.ui.mission],
+	(mission) => mission,
+);
