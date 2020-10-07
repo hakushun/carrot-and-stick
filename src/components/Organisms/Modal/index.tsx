139109151d0e71106faac902f5b069501d5c0458
Shun Nakano <nakano_syun@waku-2.com>
2020-10-07 10:33:37 +0900

    closeModal関数の実装

--- src/components/Organisms/Modal/index.tsx
+++ src/components/Organisms/Modal/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { toggle } from '../../../redux/modules/modal';
 import { Overlay } from '../Overlay';
 import styles from './index.module.scss';
 
 export const Modal: React.FC = React.memo(({ children }) => {
-	// close modalの関数作る
+	const dispatch = useDispatch();
+
+	const closeModal = useCallback(
+		(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+			e.preventDefault();
+			dispatch(toggle());
+		},
+		[dispatch],
+	);
 	return (
 		<Overlay>
 			<div className={styles.close}>
-				<button type="button" className={styles.close__button}></button>
+				<button
+					type="button"
+					className={styles.close__button}
+					onClick={closeModal}></button>
 			</div>
 			<div className={styles.modal}>{children}</div>
 		</Overlay>
