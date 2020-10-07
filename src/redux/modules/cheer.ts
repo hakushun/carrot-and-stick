31d7bb263ffe7062870613a6121beb453cf6a316
Shun Nakano <nakano_syun@waku-2.com>
2020-10-07 10:11:41 +0900

    modules/cheerの作成

--- src/redux/modules/cheer.ts
+++ src/redux/modules/cheer.ts
@@ -8,8 +8,50 @@ export type Cheer = {
 	id: number;
 	title: string;
 	point: number;
-	memo?: string;
+	memo: string;
 	status: CheerStatus;
-	registarDate: string;
-	usedDate: string;
+	registarDate: number;
+	usedDate: number;
 };
+
+type ChangePayload = {
+	name: string;
+	value: string;
+};
+
+/**
+ * action
+ */
+const actionCreator = actionCreatorFactory();
+export const change = actionCreator<ChangePayload>('CHANGE_CHEER');
+
+const INITIAL_STATE: Cheer = {
+	id: 0,
+	title: '',
+	point: 0,
+	memo: '',
+	status: 'new',
+	registarDate: 0,
+	usedDate: 0,
+};
+
+/**
+ * reducer
+ */
+const reducer = reducerWithInitialState(INITIAL_STATE).case(
+	change,
+	(state, payload) => ({
+		...state,
+		[payload.name]: payload.value,
+	}),
+);
+
+export default reducer;
+
+/**
+ * selector
+ */
+export const selectCheer = createSelector(
+	[(state: RootState) => state.ui.cheer],
+	(cheer) => cheer,
+);
--- src/redux/modules/reducers.ts
+++ src/redux/modules/reducers.ts
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux';
 import signIn from './signIn';
 import signUp from './signUp';
 import mission from './mission';
+import cheer from './cheer';
 import missions from './missions';
 
 // 複数のreducerを一つにまとめる
 const rootReducer = combineReducers({
 	resources: combineReducers({ missions }),
-	ui: combineReducers({ signIn, signUp, mission }),
+	ui: combineReducers({ signIn, signUp, mission, cheer }),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
