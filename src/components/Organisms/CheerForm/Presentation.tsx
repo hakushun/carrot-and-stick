923e5c6b392deadbbf72b51e2e16312cfa310d2d
Shun Nakano <nakano_syun@waku-2.com>
2020-10-07 10:11:54 +0900

    CheerFormコンポーネントの修正

--- src/components/Organisms/CheerForm/Presentation.tsx
+++ src/components/Organisms/CheerForm/Presentation.tsx
@@ -4,70 +4,75 @@ import { Cheer } from '../../../redux/modules/cheer';
 
 type Props = {
 	cheer: Cheer;
+	handleChange: (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+	) => void;
 };
 
-export const CheerForm: React.FC<Props> = React.memo(() => {
-	return (
-		<form className={styles.cheer}>
-			<div>
-				<label htmlFor="cheer_title" className={styles.cheer__title}>
-					<span>ご褒美タイトル</span>
-					<textarea
-						id="cheer_title"
-						className={styles.cheer__inputTitle}
-						// type="text"
-						name="title"
-						required
-						aria-required
-						// value={cheer.title}
-						maxLength={1000}
-						onChange={() => console.log('発火')}
-					/>
-				</label>
-				<div className={styles.cheer__flex}>
-					<label htmlFor="cheer_point">
-						<span>必要ポイント</span>
-						<input
-							id="cheer_point"
-							className={styles.cheer__inputPoint}
-							type="number"
-							required
-							aria-required
-							min={0}
-							max={100}
-							// value={cheer.point}
-							onChange={() => console.log('発火')}
-						/>
-					</label>
-					<label htmlFor="cheer_memo">
-						<span>メモ</span>
+export const CheerForm: React.FC<Props> = React.memo(
+	({ cheer, handleChange }) => {
+		return (
+			<form className={styles.cheer}>
+				<div>
+					<label htmlFor="cheer_title" className={styles.cheer__label}>
+						<span className={styles.cheer__labelTitle}>ご褒美タイトル</span>
 						<textarea
-							id="cheer_memo"
-							className={styles.cheer__inputMemo}
-							// type="text"
+							id="cheer_title"
+							className={styles.cheer__textarea}
+							name="title"
 							required
 							aria-required
-							// value={cheer.memo}
+							value={cheer.title}
 							maxLength={1000}
-							onChange={() => console.log('発火')}
+							onChange={handleChange}
 						/>
 					</label>
+					<div className={styles.cheer__flex}>
+						<label htmlFor="cheer_point" className={styles.cheer__label}>
+							<span className={styles.cheer__labelTitle}>必要ポイント</span>
+							<input
+								id="cheer_point"
+								className={styles.cheer__inputPoint}
+								type="number"
+								name="point"
+								required
+								aria-required
+								min={0}
+								max={100}
+								value={cheer.point}
+								onChange={handleChange}
+							/>
+						</label>
+						<label htmlFor="cheer_memo" className={styles.cheer__label}>
+							<span className={styles.cheer__labelTitle}>メモ</span>
+							<textarea
+								id="cheer_memo"
+								className={styles.cheer__textarea_memo}
+								name="memo"
+								required
+								aria-required
+								value={cheer.memo}
+								maxLength={1000}
+								onChange={handleChange}
+							/>
+						</label>
+					</div>
 				</div>
-			</div>
-			<div className={styles.form__buttonWrapper}>
-				<button className={styles.form__button} type="button">
-					追加する
-				</button>
-				<button className={styles.form__button} type="button">
-					編集する
-				</button>
-				<button className={styles.form__button} type="button">
-					更新する
-				</button>
-				<button className={styles.form__button} type="button">
-					削除する
-				</button>
-			</div>
-		</form>
-	);
-});
+				<div className={styles.cheer__buttonWrapper}>
+					<button className={styles.cheer__button} type="button">
+						追加する
+					</button>
+					<button className={styles.cheer__button} type="button">
+						編集する
+					</button>
+					<button className={styles.cheer__button} type="button">
+						更新する
+					</button>
+					<button className={styles.cheer__button} type="button">
+						削除する
+					</button>
+				</div>
+			</form>
+		);
+	},
+);
--- src/components/Organisms/CheerForm/index.tsx
+++ src/components/Organisms/CheerForm/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { change, selectCheer } from '../../../redux/modules/cheer';
 import { Modal } from '../Modal';
 import { CheerForm as Presentation } from './Presentation';
 
 export const CheerForm: React.FC = React.memo(() => {
+	const dispatch = useDispatch();
+	const cheer = useSelector(selectCheer);
+
+	const handleChange = useCallback(
+		(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+			dispatch(change({ name: e.target.name, value: e.target.value }));
+		},
+		[dispatch],
+	);
+
 	return (
 		<Modal>
-			<Presentation />
+			<Presentation cheer={cheer} handleChange={handleChange} />
 		</Modal>
 	);
 });
