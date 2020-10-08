4f358405daf24c1032b8622371f32d42f4323f43
Shun Nakano <nakano_syun@waku-2.com>
2020-10-08 09:33:10 +0900

    modules/cheerにinitializeとeditを追加

--- src/redux/modules/cheer.ts
+++ src/redux/modules/cheer.ts
@@ -2,8 +2,13 @@ import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { createSelector } from 'reselect';
 import { RootState } from './reducers';
+import {
+	addCheerAcitons,
+	updateCheerAcitons,
+	deleteCheerAcitons,
+} from './cheers';
 
-type CheerStatus = 'new' | 'canChange' | 'canUse' | 'used';
+export type CheerStatus = 'new' | 'canChange' | 'canUse' | 'used';
 export type Cheer = {
 	id: number;
 	title: string;
@@ -19,11 +24,16 @@ type ChangePayload = {
 	value: string;
 };
 
+type EditPayload = {
+	cheer: Cheer;
+};
 /**
  * action
  */
 const actionCreator = actionCreatorFactory();
 export const change = actionCreator<ChangePayload>('CHANGE_CHEER');
+export const initializeCheer = actionCreator('INITIALAIZE_CHEER');
+export const editCheer = actionCreator<EditPayload>('EDIT_CHEER');
 
 const INITIAL_STATE: Cheer = {
 	id: 0,
@@ -38,13 +48,16 @@ const INITIAL_STATE: Cheer = {
 /**
  * reducer
  */
-const reducer = reducerWithInitialState(INITIAL_STATE).case(
-	change,
-	(state, payload) => ({
+const reducer = reducerWithInitialState(INITIAL_STATE)
+	.case(change, (state, payload) => ({
 		...state,
 		[payload.name]: payload.value,
-	}),
-);
+	}))
+	.case(initializeCheer, () => ({ ...INITIAL_STATE }))
+	.case(addCheerAcitons.done, () => ({ ...INITIAL_STATE }))
+	.case(editCheer, (_state, payload) => ({ ...payload.cheer }))
+	.case(updateCheerAcitons.done, () => ({ ...INITIAL_STATE }))
+	.case(deleteCheerAcitons.done, () => ({ ...INITIAL_STATE }));
 
 export default reducer;
 
